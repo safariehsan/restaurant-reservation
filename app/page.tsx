5c74ee223f522a6cf6a8172487a6a3efcf26c793
Ehsan Safari <ehsansafari@hotmail.com>
2023-02-16 19:35:43 +0330

    create header section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="m-auto bg-white max-w-screen-lg">
+      <main className="m-auto bg-white max-w-screen-xlg">
         {/* NAVBAR */}
         <nav className="bg-white p-2 flex justify-between">
           <a href="" className="font-bold text-gray-700 text-2xl">
@@ -19,6 +19,14 @@ export default function Home() {
           </div>
         </nav>
         {/* NAVBAR */}
+        {/* HEADER */}
+        <header className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] flex text-center flex-col justify-center items-center">
+          <h1 className="text-4xlg text-white font-bold mb-4">
+            Find your table for any occasion
+          </h1>
+          <button className="bg-gray-100 rounded px-2 py-1">Reserve</button>
+        </header>
+        {/* HEADER */}
       </main>
     </main>
   );
