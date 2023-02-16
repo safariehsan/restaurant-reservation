81c77f23aedc39347d3f9722a5bc30c8a24efcee
Ehsan Safari <ehsansafari@hotmail.com>
2023-02-16 19:12:30 +0330

    create navbar section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h2 className="text-gray-500 text-xsm font-semibold">Hi there!</h2>
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="m-auto bg-white max-w-screen-lg">
+        {/* NAVBAR */}
+        <nav className="bg-white p-2 flex justify-between">
+          <a href="" className="font-bold text-gray-700 text-2xl">
+            Restaurant
+          </a>
+          <div>
+            <div className="flex">
+              <button className="bg-indigo-400 text-white px-2 py-1 text-base rounded hover:bg-indigo-500 transition-all duration-500">
+                Sign in
+              </button>
+              <button className="text-gray-600 px-2 py-1 text-base rounded hover:text-gray-800 transition-all duration-500">
+                Sign up
+              </button>
+            </div>
+          </div>
+        </nav>
+        {/* NAVBAR */}
+      </main>
     </main>
   );
 }
