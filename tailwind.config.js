68fd84dd9d08813e5d4337417e98219dce44ae08
Ehsan Safari <ehsansafari@hotmail.com>
2023-02-16 18:45:54 +0330

    install & config tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontSize: {
+      xsm: "10px",
+      sm: "12px",
+      base: "14px",
+      md: "16px",
+      lg: "18px",
+      xlg: "20px",
+    },
+  },
+  plugins: [],
+};
