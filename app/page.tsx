f692acc7b35c5ec6461a6bb2ec196c2135526ab9
Ehsan Safari <ehsansafari@hotmail.com>
2023-02-17 10:12:54 +0330

    design image gallery

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="bg-gray-200 min-h-screen w-screen">
+    <main className="bg-gray-200 min-h-screen w-screen overflow-hidden">
       <main className="m-auto bg-white max-w-screen-2xlg">
         <nav className="bg-white p-2 flex justify-between">
           <a href="" className="font-bold text-gray-700 text-2xl">
@@ -17,18 +17,18 @@ export default function Home() {
             </div>
           </div>
         </nav>
-        <header className="relative h-96 overflow-hidden bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] flex text-center flex-col justify-center items-center">
+        <header className="relative h-96 bg-center overflow-hidden bg-gradient-to-r from-[#0f1f47] to-[#5f6984] flex text-center flex-col justify-center items-center">
           <img
             src="./photo-lg.jpg"
             className="absolute w-full opacity-50"
             alt="banner"
           />
-          <h1 className="z-20 text-5xlg drop-shadow-md text-white capitalize text-center">
-            Restaurant Name (Toronto)
+          <h1 className="z-20 text-5xlg drop-shadow-md text-white capitalize text-center font-semibold">
+            Chloelys at The Hilton (Toronto)
           </h1>
         </header>
         <article className="flex m-auto w-2/3 justify-between items-start -mt-12 relative">
-          <div className="bg-white w-[70%] rounded p-3 shadow-lg h-72">
+          <div className="bg-white w-[70%] rounded p-3 shadow-lg">
             <nav className="flex justify-start border-b-2 border-gray-200 gap-6 pb-3 ">
               <a
                 href=""
@@ -55,9 +55,9 @@ export default function Home() {
                 Reviews
               </a>
             </nav>
-            <h1 className="text-4xlg font-bold py-5">
+            <h2 className="text-4xlg font-bold py-5">
               Restaurant Name Here...
-            </h1>
+            </h2>
             <div className="flex justify-start gap-6">
               <div className="rate flex gap-2">
                 <span>*****</span>
@@ -75,6 +75,36 @@ export default function Home() {
                 provides an exclusive and intimate dining experience of the
                 highest caliber, using only the finest ingredients.
               </p>
+            </div>
+            <h3 className="text-md font-semibold pt-3 border-b-2 border-gray-100 pb-2">
+              7 Photos
+            </h3>
+            <nav className="flex justify-start border-b-2 border-gray-200 gap-6 py-3">
+              <a
+                href=""
+                className="text-base text-gray-500 hover:text-gray-900 transition-all duration-500 font-semibold"
+              >
+                All
+              </a>
+              <a
+                href=""
+                className="text-base text-gray-500 hover:text-gray-900 transition-all duration-500 font-semibold"
+              >
+                Inside
+              </a>
+              <a
+                href=""
+                className="text-base text-gray-500 hover:text-gray-900 transition-all duration-500 font-semibold"
+              >
+                Outside
+              </a>
+            </nav>
+            <div className="grid grid-rows-2 grid-flow-col gap-2 mt-3">
+              <img src="./img1.png" className="rounded row-span-2 col-span-3" />
+              <img src="./img2.png" className="rounded col-span-2" />
+              <img src="./img3.png" className="rounded col-span-2" />
+              <img src="./img4.png" className="rounded col-span-2" />
+              <img src="./img5.jpg" className="rounded col-span-2" />
             </div>
           </div>
         </article>
