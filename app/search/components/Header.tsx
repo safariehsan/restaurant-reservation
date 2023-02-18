55e0ff8680957f4e7b04ec6fc353a6eb85da3127
rahkarino <ehsansafari@hotmail.com>
2023-02-18 19:55:38 +0330

    define client components

--- app/search/components/Header.tsx
+++ app/search/components/Header.tsx
@@ -1,18 +1,10 @@
 import React from "react";
+import SearchBar from "../../components/SearchBar";
 
 const Header = () => {
   return (
     <header className="h-24 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] flex text-center flex-col justify-center items-center">
-      <div className="flex flex-row">
-        <input
-          className="text-left p-2 w-[350px] flex justify-center rounded mr-2"
-          type="text"
-          placeholder="City, State, ..."
-        />
-        <button className="bg-red-600 text-white rounded px-4 py-1 text-lg font-semibold">
-          Search
-        </button>
-      </div>
+      <SearchBar />
     </header>
   );
 };
