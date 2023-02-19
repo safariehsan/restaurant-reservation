6730c0a9b40d3761fd6ccaad590c1e98c16d338f
rahkarino <ehsansafari@hotmail.com>
2023-02-19 15:40:53 +0330

    add head file + create layout components

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,23 +1,19 @@
 import React from "react";
-import NavBar from "../../../components/NavBar";
 import Header from "../components/Header";
 import RestaurantMenu from "../components/Menu";
 import RestaurantNavBar from "../components/RestaurantNavBar";
 import SubTitle from "../components/SubTitle";
+import RestaurantLayout from "../layout";
 
 const Menu = () => {
   return (
-    <main className="bg-gray-200 min-h-screen w-full">
-      <NavBar />
-      <Header />
-      <div className="mx-auto relative -mt-12 container px-20">
-        <article className="justify-between items-start bg-white rounded p-3 shadow-lg">
-          <RestaurantNavBar />
-          <SubTitle>Menu</SubTitle>
-          <RestaurantMenu />
-        </article>
-      </div>
-    </main>
+    <RestaurantLayout>
+      <article className="justify-between items-start bg-white rounded p-3 shadow-lg">
+        <RestaurantNavBar />
+        <SubTitle>Menu</SubTitle>
+        <RestaurantMenu />
+      </article>
+    </RestaurantLayout>
   );
 };
 
